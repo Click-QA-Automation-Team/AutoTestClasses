6e9a3cea13155f31a957807f70d28a2f8f853425
Ruslan <yeahbrad1897@gmail.com>
2023-01-23 12:01:38 +0500

    Updating 'SecondPartOfTests.java' class

--- SecondPartOfTests.java
+++ SecondPartOfTests.java
@@ -1,19 +1,14 @@
 import io.appium.java_client.AppiumDriver;
-import io.appium.java_client.TouchAction;
 import io.appium.java_client.android.AndroidDriver;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.openqa.selenium.By;
-import org.openqa.selenium.Dimension;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.URL;
 
-public class SecondPartOfTests {
+public class SecondPartOfTests extends NecessaryMethodsClass{
     AppiumDriver appiumDriver;
     @Before
     public void setUp() throws Exception{
@@ -34,57 +29,29 @@ public class SecondPartOfTests {
         appiumDriver.quit();
     }
     @Test
-    public void scrollTest(){
-        findElementAndClick(
+    public void windowScrollTest(){
+        waitForElementAndClick(
+                appiumDriver,
                 By.id("org.wikipedia:id/search_container"),
                 "Cannot find 'search_container' element by id",
                 5
         );
-        findElementAndSendKeys(
+        waitForElementAndSendKeys(
+                appiumDriver,
                 By.id("org.wikipedia:id/search_src_text"),
                 "Java",
                 "Cannot find 'search_src_text' element by id",
                 5
         );
-        findElementAndClick(
+        waitForElementAndClick(
+                appiumDriver,
                 By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']" +
                         "//*[@text='Java (programming language)']"),
                 "Cannot find 'search_src_text' element by id",
                 5
         );
-        scrollUpMethod(1000);
-        scrollUpMethod(1000);
-        scrollUpMethod(1000);
-    }
-    private WebElement waitMethod(By by, String errorMessage, long timeoutInSeconds){
-        WebDriverWait webDriverWait = new WebDriverWait(appiumDriver, timeoutInSeconds);
-        webDriverWait.withMessage(errorMessage);
-        return webDriverWait.until(
-                ExpectedConditions.presenceOfElementLocated(by)
-        );
-    }
-    private WebElement findElementAndClick(By by, String errorMessage, long timeoutInSeconds){
-        WebElement webElement = waitMethod(by, errorMessage, timeoutInSeconds);
-        webElement.click();
-        return webElement;
-    }
-    private WebElement findElementAndSendKeys(By by, String sendValue, String errorMessage, long timeoutInSeconds){
-        WebElement webElement = waitMethod(by, errorMessage, timeoutInSeconds);
-        webElement.sendKeys(sendValue);
-        return webElement;
-    }
-    protected void scrollUpMethod(int scrollDuration){
-        TouchAction touchAction = new TouchAction(appiumDriver);
-        // Set coordinates for scroll
-        // Getting window size of device
-        Dimension deviceWindowSize = appiumDriver.manage().window().getSize();
-        // Set center-point of width
-        int x = deviceWindowSize.width / 2;
-        // from 80%-point of device window
-        int startForY = (int) (deviceWindowSize.height * 0.8);
-        // to 20%-point of device window
-        int endForY = (int) (deviceWindowSize.height * 0.2);
-
-        touchAction.press(x, startForY).waitAction(scrollDuration).moveTo(x, endForY).release().perform();
+        scrollUpMethod(appiumDriver,1000);
+        scrollUpMethod(appiumDriver,1000);
+        scrollUpMethod(appiumDriver, 1000);
     }
 }
