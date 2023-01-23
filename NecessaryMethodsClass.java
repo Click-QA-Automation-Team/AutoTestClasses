af696f547edb9a6edd22833baa2f460f4a4863c1
Ruslan <yeahbrad1897@gmail.com>
2023-01-23 22:32:32 +0500

    Updating 'NecessaryMethodsClass.java' class

--- NecessaryMethodsClass.java
+++ NecessaryMethodsClass.java
@@ -6,6 +6,8 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
+import java.util.List;
+
 public class NecessaryMethodsClass {
     protected WebElement waitMethod(AppiumDriver appiumDriver, By by, String errorMessage, long timeoutInSeconds){
         WebDriverWait webDriverWait = new WebDriverWait(appiumDriver, timeoutInSeconds);
@@ -56,5 +58,38 @@ public class NecessaryMethodsClass {
         // to 20%-point of device window
         int endForY = (int) (deviceWindowSize.height * 0.2);
         touchAction.press(x, startForY).waitAction(scrollDuration).moveTo(x, endForY).release().perform();
+    }
+    protected void scrollUpToElementMethod(AppiumDriver appiumDriver, By by, String errorMessage, int maxNumberOfSwipes){
+        int swipes = 0;
+        // Scroll till element will be found
+        while (appiumDriver.findElements(by).size() == 0){
+            // Prevent unreachable situation
+            if(swipes > maxNumberOfSwipes){
+                waitMethod(appiumDriver,
+                        by,
+                        "Cannot find element\n"+errorMessage,
+                        0
+                );
+                return;
+            }
+            scrollUpMethod(appiumDriver, 200);
+            ++swipes;
+        }
+    }
+    protected List<WebElement> waitForElements(AppiumDriver appiumDriver, By by, String errorMessage, long timeoutInSeconds){
+        WebDriverWait webDriverWait = new WebDriverWait(appiumDriver, timeoutInSeconds);
+        webDriverWait.withMessage(errorMessage+"\n");
+        return webDriverWait.until(
+                ExpectedConditions.presenceOfAllElementsLocatedBy(by)
+        );
+    }
+    protected boolean textContains(List<WebElement> listOfElements){
+        for(int i = 0; i < listOfElements.size(); i++){
+            if(listOfElements.get(i).getText().contains("Java") ||
+                    listOfElements.get(i).getText().contains("java")){
+                return true;
+            }
+        }
+        return false;
     }
 }
