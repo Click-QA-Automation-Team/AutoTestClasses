7a0b6a1935d50e9132c2d893d1522c4ca7c78a34
Ruslan <yeahbrad1897@gmail.com>
2023-01-26 20:54:36 +0500

    Adding 'ArticleTitleExistTest.java' class

---
+++ ArticleTitleExistTest.java
@@ -0,0 +1,60 @@
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.URL;
+
+public class ArticleTitleExistTest extends NecessaryMethodsClass{
+    private AppiumDriver appiumDriver;
+    @Before
+    public void setUp() throws Exception{
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+
+        capabilities.setCapability("platformName", "Android");
+        capabilities.setCapability("deviceName", "GoogleNexus4");
+        capabilities.setCapability("platformVersion", "10");
+        capabilities.setCapability("automationName", "Appium");
+        capabilities.setCapability("appPackage", "org.wikipedia");
+        capabilities.setCapability("appActivity", ".main.MainActivity");
+        capabilities.setCapability("app", "C:\\Users\\Ruslan\\Desktop\\Automation\\Tools\\org.wikipedia.apk");
+
+        appiumDriver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+    }
+    @After
+    public void tearDown(){
+        appiumDriver.quit();
+    }
+    @Test
+    public void attributeExistTest(){
+        waitForElementAndClick(
+                appiumDriver,
+                By.id("org.wikipedia:id/search_container"),
+                //By.id("org.wikipedia:id/horizontal_scroll_list_item_image"),
+                "Cannot find 'search_container' element by its id",
+                5
+        );
+        waitForElementAndSendKeys(
+                appiumDriver,
+                By.id("org.wikipedia:id/search_src_text"),
+                "Java",
+                "Cannot find 'search_src_text' element by its id",
+                5
+        );
+        waitForElementAndClick(
+                appiumDriver,
+                By.xpath("//*[@resource-id='org.wikipedia:id/page_list_item_container']" +
+                        "//*[@text='Java (programming language)']"),
+                "Cannot find 'page_list_item_container' element by its xpath",
+                5
+        );
+        assertTitleElementPresent(
+                appiumDriver,
+                By.id("org.wikipedia:id/view_page_title_text")
+        );
+    }
+}
